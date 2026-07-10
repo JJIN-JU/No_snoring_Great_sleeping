2292572369a22770ea665262139191fa3989546e
jungho-Kang <rkdwjdgh08@gmail.com>
2026-07-10 09:44:11 +0900

    [feat] 코골이 탭 작업중

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -35,7 +35,6 @@ class SleepCareApp extends StatefulWidget {
 class _SleepCareAppState extends State<SleepCareApp> {
   final AppState _state = AppState();
   bool _showSplash = true;
-
   @override
   void dispose() {
     _state.dispose();
